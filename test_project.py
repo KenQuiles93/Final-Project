49e0ebafda81e08626f6bae55b771a26933112ca
KenQuiles93 <155103960+KenQuiles93@users.noreply.github.com>
2024-04-26 10:56:35 -0400

    Update test_project.py

--- test_project.py
+++ test_project.py
@@ -1,1 +1,16 @@
+#March 21, 2024
+#Final Project Test
+
+from project import calculate_area_circle, solve_quadratic, calculate_volume_sphere
+
+def test_calculate_area_circle():
+    assert calculate_area_circle(10) == 314.1592653589793
+
+def test_solve_quadratic():
+    assert solve_quadratic(1, -3, 2) == (2.0, 1.0)
+    assert solve_quadratic(1, 2, 1) == -1.0
+    assert solve_quadratic(1, 0, 1) == "No real roots"
+
+def test_calculate_volume_sphere():
+    assert calculate_volume_sphere(10) == 4188.790204786391
 
